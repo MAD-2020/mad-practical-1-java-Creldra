fe915f3a54a49f3e9c8590ee0bf0a862c1ef8b5f
Creldra <64131631+Creldra@users.noreply.github.com>
2020-04-22 20:42:35 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
+    ArrayList<Integer> numList = new ArrayList<Integer>();
+
+    //gets a number input from the user
+    Scanner userInput = new Scanner(System.in);
+    System.out.print("Enter a Number: ");
+    int num = userInput.nextInt();
+
+    int counter = 1;
+    while(num != 0){
+      Scanner userInputs = new Scanner(System.in);
+      System.out.print(counter + ") Enter a Number: " );
+      int numInput = userInputs.nextInt();
+      //System.out.print("\n");
+      numList.add(numInput);
+      counter ++;
+      num --;
+    }
     
+    //use mappings
+    //NOT DONE YET
   }
 }
