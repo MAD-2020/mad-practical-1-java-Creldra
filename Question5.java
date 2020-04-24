f4b1d5bbba56b568878b89b5a1a7ac753b5f54ef
Creldra <64131631+Creldra@users.noreply.github.com>
2020-04-24 11:17:29 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Map.Entry; 
+import java.util.*; 
 
 public class Question5
 {
@@ -12,6 +16,7 @@ public class Question5
     System.out.print("Enter a Number: ");
     int num = userInput.nextInt();
 
+    //get the user to intput an amount of number depending on the first input
     int counter = 1;
     while(num != 0){
       Scanner userInputs = new Scanner(System.in);
@@ -22,8 +27,24 @@ public class Question5
       counter ++;
       num --;
     }
-    
-    //use mappings
-    //NOT DONE YET
+
+    //the mapping function that helps mapping same value together and also able to get the frequencies of this value occuring
+    Map<Integer, Integer> mapper = new HashMap<Integer, Integer>();
+    for(Integer j : numList){
+      Integer k = mapper.get(j);
+      mapper.put(j, (k == null) ? 1 : k + 1);
+    }
+
+    //to only get the MOST occurence number (this code could only show 1 most occurence and not more than that, meaning if there is 2 numbers with the same frequencies, it will only show the lowest number)
+    Integer checker = 0;
+    Integer lastNum = 0;
+    for(Map.Entry<Integer,Integer> val : mapper.entrySet()){
+      if(val.getValue() > checker){
+        checker = val.getValue();
+        lastNum = val.getKey();
+      }
+    }
+
+    System.out.println("The Most Occurence Number: " + lastNum);
   }
 }
